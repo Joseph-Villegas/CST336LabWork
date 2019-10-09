324867c9b84dc137b802e377f93501ccfc989ce9
Kevin Guzman <kevin@apthos.net>
2019-10-08 23:35:28 -0700

    10 - 8 - 2019

--- public/labs/Lab5/js/functions.js
+++ public/labs/Lab5/js/functions.js
@@ -1,5 +1,27 @@
 $(document).ready(function (){
 
+    $("#search").on('click', submitSearch);
+
+    function submitSearch() {
+        let query = $("#query").val();
+        let orientation = $("#orientation").val();
+        console.log(query, orientation);
+
+        if (query === "") {
+            // no input entered
+            console.log("NO QUERY TEXT");
+            return;
+        }
+
+        if (orientation === "") {
+            // no orientation entered
+            console.log("NO ORIENTATION SELECTED");
+            return;
+        }
+
+        getImages(query, orientation, "13832434-d1a24747950bb1a91b3a9f59c")
+    }
+
     function getImages(searchText, orientation, apiKey) {
         let url_pix = 'https://pixabay.com/api/';
         $.ajax({
@@ -20,6 +42,13 @@ $(document).ready(function (){
     }
 
     function process_image_results(response) {
-        console.log("response", response);
+        let hits = response["hits"];
+        let img_count = 4;
+
+        for (let i = 0; i < img_count; i++) {
+            let img_data = hits[i];
+            console.log(img_data);
+            $(`#img${i+1}`).attr("src", img_data["webformatURL"]);
+        }
     }
 });
