38004f55ad2d452d41b0777f163daea1af037be5
Joseph Villegas <jovillegas@csumb.edu>
2019-11-05 01:07:10 +0000

    lab7

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/labs/6', lab_6_router);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
